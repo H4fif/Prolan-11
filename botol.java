9a5615025e332027f38fb1599805ff6d24e1ff2f
Hafif Imammuddyn <hafifimammuddyn@gmail.com>
2017-03-19 08:20:32 +0700

    Tugas 4 - Kelas dan Objek

---
+++ botol.java
@@ -0,0 +1,7 @@
+public class botol {
+	public String merk, ukuran;
+	
+	public void tampung_air () {
+		System.out.println ("Botol tampung air");
+	}
+}
---
+++ galon.java
@@ -0,0 +1,7 @@
+public class galon {
+	public String merk, ukuran, jenis_air;
+	
+	public void tampung_air () {
+		System.out.println ("Galon tampung air");
+	}
+}

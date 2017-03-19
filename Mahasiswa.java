b18a8051e575628a5dba903d2eb17a48dc1a1315
Hafif Imammuddyn <hafifimammuddyn@gmail.com>
2017-03-19 10:32:32 +0700

    Delete Mahasiswa.java

--- Mahasiswa.java
+++
@@ -1,7 +0,0 @@
-class Mahasiswa {
-    public static void main(String[] args) {
-        System.out.println("Nama   : Hafif Imammuddyn");
-	System.out.println("NIM    : 10114337");
-	System.out.println("Alamat : Jl. Cisokan Baru No. 9");
-    }
-}

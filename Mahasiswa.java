a8d73e193976dd6acbead5ce115015f1656a6299
Hafif Imammuddyn <hafifimammuddyn@gmail.com>
2017-03-19 08:17:47 +0700

    Tugas 2 - Mahasiswa

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -1,7 +1,7 @@
 class Mahasiswa {
     public static void main(String[] args) {
         System.out.println("Nama   : Hafif Imammuddyn");
-		System.out.println("NIM    : 10114337");
-		System.out.println("Alamat : Jl. Cisokan Baru No. 9");
+	System.out.println("NIM    : 10114337");
+	System.out.println("Alamat : Jl. Cisokan Baru No. 9");
     }
 }

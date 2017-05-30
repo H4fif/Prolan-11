36332584d8292d864cd3a806034fc756ed78e86d
Hafif Imammuddyn <hafifimammuddyn@gmail.com>
2017-05-30 22:25:37 +0700

    Tugas 14 - Generic Class

---
+++ Tugas 14/DiskonMember.java
@@ -0,0 +1,20 @@
+public class DiskonMember<T, N> {
+	public T a;
+	public N b;
+	
+	public T get_a(){
+		return a;
+	}
+	
+	public N get_b(){
+		return b;
+	}
+	
+	public void set_a(){
+		this.a = a;
+	}
+	
+	public void set_b(N disc){
+		this.b = disc;
+	}
+}
---
+++ Tugas 14/Readme.txt
@@ -0,0 +1,6 @@
+<p>
+	Implementasi class <i>generic</i> pada <i><b>DiskonMember.java<b></i>
+</p>
+<p>
+	Digunakan di <i>class <b>main</b></i>
+</p>
